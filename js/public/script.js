395adb014e823b57735b563dc854bd2d0277be07
Masha Martyniuk <mmartyniuk004@gmail.com>
2023-04-04 08:58:51 +0300

    added three.js project

---
+++ js/public/script.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  let canvas = document.getElementById('canvas');
+
+  canvas.setAttribute('width', width);
+  canvas.setAttribute('height', height);
+
+  let renderer = new THREE.WebGLRenderer({ canvas: canvas });
+  renderer.setClearColor(0x000000);
+
+  let scene = new THREE.Scene();
+
+  let camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
+  camera.position.set(0, 0, 1000);
+
+  let light = new THREE.AmbientLight(0xffffff);
+  scene.add(light);
+
+  let geometry = new THREE.SphereGeometry(200, 12, 12);
+  let material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    wireframe: true,
+  });
+  let mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  function loop() {
+    mesh.rotation.y += Math.PI / 500;
+    renderer.render(scene, camera);
+    requestAnimationFrame(function () {
+      loop();
+    });
+  }
+
+  loop();
+};
